3aba343de3a518285cd263a187b4a68695947c85
Keyur <keyurdakbari@gmail.com>
2018-06-08 19:01:34 +0530

    Version 1.0 Build 1.2

--- Tutable/Custom Class/GlobalConstant.swift
+++ Tutable/Custom Class/GlobalConstant.swift
@@ -10,8 +10,10 @@ import Foundation
 import UIKit
 
 //Live
-let BASE_URL = "https://backend.tutable.com.au/production/api/"
+//let BASE_URL = "https://backend.tutable.com.au/production/api/"
 //let BASE_URL = "https://backend.tutable.com.au/development/api/"
+let BASE_URL = "https://betutable.com.au/production/api/"
+
 let PHOTO_BASE_URL = BASE_URL
 let CERTIFICATE_URL = BASE_URL + "api/certificates/asset/"
 let CLASS_URL = BASE_URL + "api/class/assets/"
