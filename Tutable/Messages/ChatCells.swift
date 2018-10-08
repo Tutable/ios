f2f72e9ec9dc9ad1c0c037099ed4ace300209d7c
Rohit Saini <sketchlearn7@gmail.com>
2018-10-08 10:43:17 +0530

    New Sender and Receiver chat cells added

--- Tutable/Messages/ChatCells.swift
+++ Tutable/Messages/ChatCells.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+import UIKit
+
+class SenderCell: UITableViewCell {
+    
+    @IBOutlet weak var profilePic: RoundedImageView!
+    @IBOutlet weak var message: UITextView!
+    @IBOutlet weak var messageBackground: UIImageView!
+    
+    func clearCellData()  {
+        self.message.text = nil
+        self.message.isHidden = false
+        self.messageBackground.image = nil
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.selectionStyle = .none
+        self.message.textContainerInset = UIEdgeInsetsMake(5, 5, 5, 5)
+        self.messageBackground.layer.cornerRadius = 15
+        self.messageBackground.clipsToBounds = true
+    }
+}
+
+class ReceiverCell: UITableViewCell {
+    
+    @IBOutlet weak var profilePic: RoundedImageView!
+    @IBOutlet weak var message: UITextView!
+    @IBOutlet weak var messageBackground: UIImageView!
+    
+    func clearCellData()  {
+        self.message.text = nil
+        self.message.isHidden = false
+        self.messageBackground.image = nil
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.selectionStyle = .none
+        self.message.textContainerInset = UIEdgeInsetsMake(5, 5, 5, 5)
+        self.messageBackground.layer.cornerRadius = 15
+        self.messageBackground.clipsToBounds = true
+    }
+}
+
+
