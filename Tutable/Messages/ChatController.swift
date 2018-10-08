33c63399c0a9aa4299989112f7675487edf2da9c
Rohit Saini <sketchlearn7@gmail.com>
2018-10-08 10:38:12 +0530

    Chat Model Classes and chat controller Classed Added

---
+++ Tutable/Messages/ChatController.swift
@@ -0,0 +1,9 @@
+//
+//  ChatController.swift
+//  Tutable
+//
+//  Created by Rohit Saini on 05/10/18.
+//  Copyright © 2018 Keyur. All rights reserved.
+//
+
+import Foundation
---
+++ Tutable/Messages/ChatModel/Conversation.swift
@@ -0,0 +1,9 @@
+//
+//  Conversation.swift
+//  Tutable
+//
+//  Created by Rohit Saini on 04/10/18.
+//  Copyright © 2018 Keyur. All rights reserved.
+//
+
+import Foundation
