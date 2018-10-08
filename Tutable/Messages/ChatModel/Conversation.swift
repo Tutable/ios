787143af2e451294c91713b96a97d16416e54040
Rohit Saini <sketchlearn7@gmail.com>
2018-10-08 10:46:15 +0530

    Chat Model Classes Added(User,messages,conversions)

--- Tutable/Messages/ChatModel/Conversation.swift
+++ Tutable/Messages/ChatModel/Conversation.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+
+
+import Foundation
+import UIKit
+import Firebase
+
+class Conversation {
+    
+    //MARK: Properties
+    let user: USER
+    var lastMessage: Messages
+    
+    //MARK: Methods
+    class func showConversations(completion: @escaping ([Conversation]) -> Swift.Void) {
+        if let currentUserID = AppModel.shared.currentUser.id {
+            var conversations = [Conversation]()
+            Database.database().reference().child("users").child(currentUserID).child("conversations").observe(.childAdded, with: { (snapshot) in
+                if snapshot.exists() {
+                    let fromID = snapshot.key
+                    let values = snapshot.value as! [String: String]
+                    let location = values["location"]!
+                    USER.info(forUserID: fromID, completion: { (user) in
+                        let emptyMessage = Messages.init(type: .text, content: "loading", owner: .sender, timestamp: 0, isRead: true)
+                        let conversation = Conversation.init(user: user, lastMessage: emptyMessage)
+                        conversations.append(conversation)
+                        conversation.lastMessage.downloadLastMessage(forLocation: location, completion: {
+                            completion(conversations)
+                        })
+                    })
+                }
+            })
+        }
+    }
+    
+    //MARK: Inits
+    init(user: USER, lastMessage: Messages) {
+        self.user = user
+        self.lastMessage = lastMessage
+    }
+}
+
